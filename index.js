800bf069467aa4a5121b99537a41d618cae5aa02
AyubuAyubu <ayubu6143@gmail.com>
2022-08-19 09:25:39 +0300

    Completed Task

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+//mutiply assignment
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+//random Assignment
+let random = Math.floor( Math.random() * 10 ) + 1;
+
+//modulus Assignment
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+//Max Assignment
+let max = Math.max(1,20,9,13);
